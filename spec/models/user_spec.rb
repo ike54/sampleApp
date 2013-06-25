0eee6beb3d28409a709135a667cf55ad6a81ad83
Isaac <ikecollege@gmail.com>
2013-06-25 13:51:32 -0500

    Start Ch.7

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -31,7 +31,7 @@ describe User do
 		it { should_not be_valid}
 	end
 
-	  describe "when email format is invalid" do
+  describe "when email format is invalid" do
     it "should be invalid" do
       addresses = %w[user@foo,com user_at_foo.org example.user@foo.
                      foo@bar_baz.com foo@bar+baz.com]
@@ -52,7 +52,7 @@ describe User do
     end
   end
 
-    describe "when email address is already taken" do
+  describe "when email address is already taken" do
     before do
       user_with_same_email = @user.dup
       user_with_same_email.save
@@ -62,6 +62,16 @@ describe User do
     it { should_not be_valid }
   end
 
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
+  end
+  
   describe "when password is not present" do
     before do
         @user = User.new(name: "Example User", email: "user@example.com",
