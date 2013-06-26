637aafcb804ae5078f050a8fdb6ba9fc857a9e30
Isaac <ikecollege@gmail.com>
2013-06-26 03:27:53 -0500

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,11 +8,13 @@ describe User do
 
 	subject { @user }
 
+
 	it { should respond_to(:name) }
 	it { should respond_to(:email) }
 	it { should respond_to(:password_digest) }
 	it { should respond_to(:password) }
 	it { should respond_to(:password_confirmation)}
+  it { should respond_to(:remember_token) }
 	it { should respond_to(:authenticate) }
 	it { should be_valid }
 
@@ -71,7 +73,7 @@ describe User do
       expect(@user.reload.email).to eq mixed_case_email.downcase
     end
   end
-  
+
   describe "when password is not present" do
     before do
         @user = User.new(name: "Example User", email: "user@example.com",
@@ -114,4 +116,8 @@ describe User do
     it { should be_invalid }
   end
   
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
 end
